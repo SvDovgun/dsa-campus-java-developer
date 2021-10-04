29c7df1c773e28d9cfd6e45c6b66a80371afeded
SvDovgun <sv.dovgun@gmail.com>
2021-10-04 13:57:24 +0300

    Tasks about Triangles

---
+++ Tasks/Triangles.java
@@ -0,0 +1,74 @@
+class Triangles {
+
+	public static void main (String[] args){
+		System.out.println("Triangle 1 ");
+		// for (init; condition; operation)
+		for (int i = 0; i < 4 ; i++){
+			for (int j = 0; j < 4 ; j++){
+				if (j<=i){
+					System.out.print("*");
+				}
+			}
+			System.out.println("");
+		}
+		/*Triangle 1
+		*
+		**
+		***
+		****
+		*/
+		
+		System.out.println("Triangle 2 ");
+		// for (init; condition; operation)
+		for (int i = 0; i < 4 ; i++){
+			for (int j = 0; j < 4 ; j++){
+				if (i>j){
+					System.out.print(" ");
+				}else {System.out.print("*");}
+			}
+			System.out.println("");
+		}
+		/*Triangle 2
+		****
+		 ***
+		  **
+		   *
+		*/
+		
+		System.out.println("Triangle 3 ");
+		// for (init; condition; operation)
+		for (int i = 0; i < 4 ; i++){			
+			for (int j = 3; j >= 0 ; j--){      
+				if (j<=i){
+					System.out.print("*");
+				} else {System.out.print(" ");}
+			}
+			System.out.println("");
+		}
+		/*Triangle 3
+		   *
+		  **
+		 ***
+		****
+		*/
+		
+		System.out.println("Triangle 4 ");
+		// for (init; condition; operation)
+		for (int i = 0; i < 4 ; i++){
+			for (int j = 0; j < 4 ; j++){
+				if (j>=i){
+					System.out.print("*");
+				}
+			}
+			System.out.println("");
+		}
+		/*Triangle 4
+		****
+		***
+		**
+		*
+		*/
+
+		System.out.println(" Finish! ");	
+	}
+}
